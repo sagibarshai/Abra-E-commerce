2eee94a3c9a8d003c13f0b357c0ec94ac69dc031
Sagi bar-shai <sagibarshai1@gmail.com>
2022-07-11 00:27:59 +0300

    commit

--- FRONTEND/src/Manager/AddProduct.js
+++ FRONTEND/src/Manager/AddProduct.js
@@ -1,50 +1,134 @@
 import React, {useState} from "react";
+import styled from "styled-components";
 import axios from 'axios'
+import { StyledPraimaryButton } from "../shared/UIElements/StyledPraimaryButton";
+import {useNavigate} from 'react-router-dom'
 const AddProduct = () => {
+  const navigate = useNavigate()
   const [name,setName] = useState('')
-  const [imageUrl,setImageUrl] = useState('')
+  const [imgSrc,setImgSrc] = useState('')
   const [price,setPrice] = useState('')
-  const [description,setDescription] = useState('')
+  const [quantity,setQuantity] = useState(5)
   const [types,setTypes] = useState([])
+  const [errors,setErrors] = useState([]) 
   const onSubmitHandler = (e) => {
+    let errorsArray = []
     e.preventDefault()
-    const data = {name , price , description , imageUrl , types}
-    axios.put(process.env.REACT_APP_BACKEND_URL + '/manager/add-product' , {
-      data
-    })
-    .then(res => console.log(res.data.data))
-    .catch(err => console.log(err))
-     
+    if(name.length <2) {
+      errorsArray.push(`name filed contain at least 2 characters`)
+    }
+    if(imgSrc.length <=10) {
+       errorsArray.push(`image src filed must contain at least 10 characters`)
+    }
+    if(price <= 0 || !price) {
+      errorsArray.push(`price filed must be above 0 !`)
+    }
+    if(quantity <= 0 || !quantity) {
+      errorsArray.push(`quantity field must be above 0 !`)
+    }
+    if(types.length <= 0) {
+      errorsArray.push(`you must choose at least 1 type`)
+    }
+    setErrors(errorsArray)
+    if(errorsArray.length === 0) {
+      const data = {name , price  , imgSrc ,quantity , types}
+      axios.put(process.env.REACT_APP_BACKEND_URL + '/manager/add-product' , data)
+      .then(res => navigate('/manager/products')
+      )
+      .catch(err => console.log(err))
+    }
+    }
+
+const checkboxHandler = (id,value) => {
+  if(document.getElementById(id).checked)  setTypes([...types , document.getElementById(id).value])
+  else {
+    let newCheckboxState= [...types].filter(type => type !== value)
+    setTypes(newCheckboxState)
   }
-  
+}
   return (
-    <form onSubmit={(e) => onSubmitHandler(e)}>
-      <label htmlFor="name">Name</label>
-      <input value={name} onChange={(e)=> {setName(e.target.value)}} placeholder="name" id="name" />
-      <label htmlFor="imageUrl">imageUrl</label>
-      <input value={imageUrl} onChange={(e)=> {setImageUrl(e.target.value)}} placeholder="imageUrl" id="imageUrl" />
-      <label htmlFor="price">price</label>
-      <input value={price} onChange={(e)=> {setPrice(e.target.value)}} placeholder="price" id="price" />
-      <label htmlFor="description">description</label>
-      <input value={description} onChange={(e)=> {setDescription(e.target.value)}} placeholder="description" id="description" />
-      <label htmlFor="types">types</label>
-      <select onClick={() => {
-         const selected = [];
-         for (const option of document.getElementById('types').options) {
-             if (option.selected) {
-                 selected.push(option.value);
-             }}
-         setTypes(selected)
-      }} id="types" multiple >
-        <option  value="Best_Sellers">Best Sellers</option>
-        <option value="Clothing">Clothing</option>
-        <option value="Office">Office</option>
-        <option value="Home">Home</option>
-        <option value="Sport">Sport</option>
-      </select>
-      <button type="submit">Add</button>
-    </form>
+    <StyledPage>
+    <StyledForm onSubmit={(e) => onSubmitHandler(e)}>
+      {errors.length > 0 && <StyledLeabl>{errors.length} errors found</StyledLeabl>}
+      {errors.length > 0 && errors.map((error, idx) => {
+        return <StyledLeabl key={idx}>{error} <br></br></StyledLeabl>
+      })}
+      <StyledLeabl htmlFor="name">Name</StyledLeabl>
+      <StyledInput value={name} onChange={(e)=> {setName(e.target.value)}} placeholder="name" id="name" />
+      <StyledLeabl htmlFor="imageUrl">image Url</StyledLeabl>
+      <StyledInput value={imgSrc} onChange={(e)=> {setImgSrc(e.target.value)}} placeholder="image Url" id="imageSrc" />
+      <StyledLeabl htmlFor="price">price</StyledLeabl>
+      <StyledInput type="number" value={price} onChange={(e)=> {setPrice(e.target.value)}} placeholder="price" id="price" />
+      <StyledLeabl htmlFor="quantity">quantity</StyledLeabl>
+      <StyledInput type="number" value={quantity} onChange={(e)=> {setQuantity(e.target.value)}} placeholder="quantity" id="quantity" />
+     <StyledLeabl>What are the types for this product?</StyledLeabl>
+      <StyledCheckboxContainer>
+      <StyledLeabl>Best Sellers</StyledLeabl>
+      <StyledCheckBox onClick={() => {
+        checkboxHandler("Best_Sellers" , "Best_Sellers")
+      }} id="Best_Sellers" value="Best_Sellers" type="checkbox" />
+      <StyledLeabl>Home</StyledLeabl>
+      <StyledCheckBox onClick={() => {
+        checkboxHandler('Home' , 'Home')
+      }} id="Home" value="Home" type="checkbox"/>
+      <StyledLeabl>Office</StyledLeabl>
+      <StyledCheckBox onClick={() => {
+        checkboxHandler('Office' , 'Office')
+      }} id="Office" value="Office" type="checkbox"/>
+      <StyledLeabl>Sport</StyledLeabl>
+      <StyledCheckBox onClick={() => {
+        checkboxHandler('Sport' , 'Sport')
+      }} id="Sport" value="Sport" type="checkbox"/>
+      <StyledLeabl>Clothing</StyledLeabl>
+      <StyledCheckBox onClick={() => {
+        checkboxHandler('Clothing' , 'Clothing')
+      }} id="Clothing" value="Clothing" type="checkbox"/>
+
+    </StyledCheckboxContainer>
+      <StyledPraimaryButton height="50px" backgroundColor="black" color="white" hoverBackgroundColor="white" hoverColor="black" border="none" boxShadow="2px 2px 2px solid black" width="50%" type="submit">Add</StyledPraimaryButton>
+    </StyledForm>
+    </StyledPage>
   );
 };
+  const StyledPage = styled.div`
+  width:100vw;
+  background-color: white;
+`;
+const StyledCheckboxContainer = styled.div`
+display: flex;
+flex-direction: row;
+column-gap: 15px;
+`
+const StyledCheckBox = styled.input`
+width: 15px;
+height:15px;
+
+&:checked{
+color:black;
+background-color: black;
+}
+`
+const StyledForm = styled.form`
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15px;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const StyledLeabl = styled.label`
+font-size:20px;
+`;
+
+const StyledInput = styled.input`
+  width: 50%;
+  height: 40px;
+  text-align: center;
+  font-size: 20px;
+  padding: 0%;
+  margin:0;
+  `;
 
 export default AddProduct;
